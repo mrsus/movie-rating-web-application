4ae6158b10a1a2147667b24c08f0eff0e7eccb1c
Navin <navin@townscript.com>
2014-11-11 19:28:31 +0530

    adding User model

---
+++ src/main/java/com/dyulok/dewa/model/User.java
@@ -0,0 +1,54 @@
+package com.dyulok.dewa.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="User")
+public class User {
+	private int id;
+	private String name;
+	private String email;
+	
+	public User(){
+		System.out.println("in user pojo constructor...");
+	}
+
+	public User(String name, String email) {
+		super();
+		this.name = name;
+		this.email = email;
+	}
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+
+}
